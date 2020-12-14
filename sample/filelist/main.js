e209d4b4c461a03ee2e5e31ab20c0ae61b195885
dokukurage <17e1198@andrew.ac.jp>
2020-12-14 17:59:18 +0900

    コメ追加

--- sample/filelist/main.js
+++ sample/filelist/main.js
@@ -1,14 +1,19 @@
 //haritukeのほうにあるindex.jsの内容に近い
+//書き換え禁止変数（定数）宣言const
+// ファイル読み込み（require）
 const electron = require('electron');
 
+//定数を宣言(const)
 const { app, BrowserWindow } = electron;
+//変数winを新規作成(let)
 let win;
-
+// 新しいウィンドウ(Webページ)を生成
 function createWindow() {
+  // BrowserWindowインスタンスを生成
   win = new BrowserWindow({
     webPreferences: {
-      //アプリの画面の枠
-      //
+      //アプリの画面の枠＝メインプロセスでウィンドウを生成する
+      //require関数が使えるようになる？（true）
       nodeIntegration: true,
       enableRemoteModule: true
     }
@@ -16,7 +21,7 @@ function createWindow() {
   //index.htmlをロード
   win.loadURL(`file://${__dirname}/index.html`);
 
-  //ウィンドウが閉じられると発生
+  //ウィンドウが閉じられると発生(終了イベント?)
   win.on('closed', () => {
     win = null
   });
